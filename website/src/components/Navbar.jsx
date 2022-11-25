0bf46c5f1c20c075801085dfd844d619123947a8
Strryke <gavinong2000@gmail.com>
2022-11-25 15:37:44 +0800

    removed menu buttons

--- website/src/components/Navbar.jsx
+++ website/src/components/Navbar.jsx
@@ -61,24 +61,6 @@ export default function Nav() {
                     src={"https://avatars.dicebear.com/api/male/username.svg"}
                   />
                 </MenuButton>
-                <MenuList alignItems={"center"}>
-                  <br />
-                  <Center>
-                    <Avatar
-                      size={"2xl"}
-                      src={"https://avatars.dicebear.com/api/male/username.svg"}
-                    />
-                  </Center>
-                  <br />
-                  <Center>
-                    <p>Username</p>
-                  </Center>
-                  <br />
-                  <MenuDivider />
-                  <MenuItem>Your Servers</MenuItem>
-                  <MenuItem>Account Settings</MenuItem>
-                  <MenuItem>Logout</MenuItem>
-                </MenuList>
               </Menu>
             </Stack>
           </Flex>
